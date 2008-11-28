fa5e8eaa9e6ef3200e2c5237a8887fe47768f7d1
Sebastian Walter <sebastian.walter@gmail.com>
2008-11-28 15:07:06 +0100

    added more types

--- py_cppad.hpp
+++ py_cppad.hpp
@@ -1,7 +1,6 @@
 #ifndef PY_CPPAD_HPP
 #define PY_CPPAD_HPP
 
-
 #define PY_ARRAY_UNIQUE_SYMBOL PyArrayHandle
 #include "num_util.h"
 
@@ -14,15 +13,23 @@ namespace bp = boost::python;
 namespace bpn = boost::python::numeric;
 namespace nu = num_util;
 
-
+/* Base types */
 typedef CppAD::AD<double> AD_double;
-typedef CppAD::vector<double> vector_double;
-typedef CppAD::VecAD<double> VecAD_double;
 typedef CppAD::AD<CppAD::AD<double> > ADD_double;
+
+/* vector types */
+typedef CppAD::vector<double> vector_double;
 typedef CppAD::vector<CppAD::AD<double> > vector_AD_double;
-typedef CppAD::VecAD< CppAD::AD<double> > VecAD_ADdouble;
+typedef CppAD::vector<CppAD::AD<CppAD::AD<double> > > vector_ADD_double;
 
+/* ??? types */
+typedef CppAD::VecAD<double> VecAD_double;
+typedef CppAD::VecAD< CppAD::AD<double> > VecAD_AD_double;
 
+/* ADFun types */
+typedef CppAD::ADFun<double> ADFun_double;
+typedef CppAD::ADFun<CppAD::AD<double> > ADFun_AD_double;
+typedef CppAD::ADFun<CppAD::AD<CppAD::AD<double> > > ADFun_ADD_double;
 
 BOOST_PYTHON_MODULE(_cppad)
 {
@@ -33,13 +40,11 @@ BOOST_PYTHON_MODULE(_cppad)
 	scope().attr("__doc__") =" CppAD: docstring \n\
 							   next line";
 
-
-
 	class_<AD_double>("AD_double");
 	class_<VecAD_double>("VecAD_double");
 
 	def("Independent",		CppAD::Independent<vector_AD_double>);
-
+	def("Independent",		CppAD::Independent<vector_ADD_double>);
 }
 
 #endif
