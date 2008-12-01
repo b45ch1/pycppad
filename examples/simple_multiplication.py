67a91118e70491926e2d34c9ac6e49cd390ea31c
Sebastian Walter <sebastian.walter@gmail.com>
2008-12-01 16:53:18 +0100

    tidied up and added commented lines that show what else has to be wrapped

--- examples/simple_multiplication.py
+++ examples/simple_multiplication.py
@@ -1,26 +1,16 @@
 #!/usr/bin/env python
-
 import sys
 sys.path = ['./release'] + sys.path
 
 from cppad import *
 import numpy
 
-
-# simple multiplication
-ax = AD_double(1.5)
-ay = AD_double(1.7)
-az = ax*ay
-print ax
-print ay
-print az
-
 # numpy arrays of AD_double
 ax = numpy.array([AD_double(2 + 1.5*i) for i in range(2)],dtype=object)
 Independent(ax)
-print ax
 ay = numpy.array([ax[0] * ax[1]])
-print ay
-#x = MyVec(23.)
+# f = ADFun_double(ax,ay)
+#f.forward(...)
+#f.reverse(...)
 
-#test_my_vec(x)
+
