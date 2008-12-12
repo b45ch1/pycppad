f9290794665bad22c0d33cbc43fee57cfa7de1a6
Sebastian Walter <sebastian.walter@gmail.com>
2008-12-12 17:56:03 +0100

    incorporated python_cppad.sh prototype to the final wrapper

--- examples/simple_multiplication.py
+++ examples/simple_multiplication.py
@@ -2,16 +2,26 @@
 import sys
 sys.path = ['./release'] + sys.path
 
-from cppad import *
-import numpy
-
-# numpy arrays of AD_double
-ax = numpy.array([AD_double(2 + 1.5*i) for i in range(2)],dtype=object)
-Independent(ax)
-ay = numpy.array([ax[0] * ax[1]])
-
-# f = ADFun_double(ax,ay)
-#f.forward(...)
-#f.reverse(...)
-
-
+from python_cppad import *
+from numpy import array
+x = array( [ AD_double(2) , AD_double(3) ] )
+independent(x);
+y = array( [ x[0] * x[1] ] );
+f = ADFun_double(x, y)
+print 'f(x0, x1) = x0 * x1'
+p  = 0
+xp = array( [ 3. , 4. ] )
+fp = f.Forward(p, xp)
+print 'f(3, 4)            = ', fp
+p  = 1
+xp = array( [ 1. , 0. ] )
+fp = f.Forward(p, xp)
+print 'partial_x0 f(3, 4) = ' , fp
+p  = 1
+xp = array( [ 0. , 1. ] )
+fp = f.Forward(p, xp)
+print 'partial_x1 f(3, 4) = ' , fp
+print 'Begin: example python_cppad error message'
+p  = 1
+xp = array( [ 0. , 1., 0. ] )
+fp = f.Forward(p, xp)
--- py_cppad.hpp
+++ py_cppad.hpp
@@ -2,8 +2,8 @@
 #define PY_CPPAD_HPP
 
 #define PY_ARRAY_UNIQUE_SYMBOL PyArrayHandle
-#include "num_util.h"
 
+#include "num_util.h"
 #include "cppad/cppad.hpp"
 
 using namespace std;
@@ -12,34 +12,79 @@ namespace bp = boost::python;
 namespace bpn = boost::python::numeric;
 namespace nu = num_util;
 
-/* Base types */
-typedef CppAD::AD<double> AD_double;
-typedef CppAD::AD<CppAD::AD<double> > ADD_double;
-
-/* vector types */
-typedef CppAD::vector<double> vector_double;
-typedef CppAD::vector<CppAD::AD<double> > vector_AD_double;
-typedef CppAD::vector<CppAD::AD<CppAD::AD<double> > > vector_ADD_double;
-
-/* ??? types */
-typedef CppAD::VecAD<double> VecAD_double;
-typedef CppAD::VecAD< CppAD::AD<double> > VecAD_AD_double;
-
-/* ADFun types */
-typedef CppAD::ADFun<double> ADFun_double;
-typedef CppAD::ADFun<CppAD::AD<double> > ADFun_AD_double;
-typedef CppAD::ADFun<CppAD::AD<CppAD::AD<double> > > ADFun_ADD_double;
 
 
-/* elementary binary operators */
-AD_double *AD_double_mul_AD_double_AD_double(const AD_double &lhs, const AD_double &rhs);
+namespace{
+	typedef CppAD::AD<double> AD_double;
+	typedef CppAD::AD<CppAD::AD<double> > ADD_double;
 
-/* elementary unary operators */
-AD_double	(*cos_AD_double) 		( const AD_double & ) = &CppAD::cos;
+	class ADFun_double {
+		private:
+			CppAD::ADFun<double> f_;
+		public:
+			ADFun_double(bpn::array& x_array, bpn::array& y_array);
+			bpn::array Forward(int p, bpn::array& xp);
+	};
 
-/* general functions */
-void Independent_numpy_AD_double(bpn::array &bpn_x);
-AD_double *AD_double_mul_AD_double_AD_double(const AD_double &lhs, const AD_double &rhs);
+	class AD_double_vec {
+		private:
+			size_t       length_;
+			AD_double  *pointer_;
+			AD_double **handle_;
+		public:
+			typedef AD_double value_type;
+			AD_double_vec(bpn::array& py_array);
+			AD_double_vec(size_t length);
+			AD_double_vec(const AD_double_vec& vec);
+			AD_double_vec(void);
+			~AD_double_vec(void);
+			void operator=(const AD_double_vec& vec);
+			size_t size(void) const;
+			void resize(size_t length);
+			AD_double& operator[](size_t i);
+			const AD_double& operator[](size_t i) const;
+	};
+
+	class double_vec {
+		private:
+			size_t    length_;
+			double  *pointer_;
+			bool    allocated_;
+		public:
+			typedef double value_type;
+
+			double_vec(bpn::array& py_array);
+			double_vec(size_t length);
+			double_vec(const double_vec& vec);
+			double_vec(void);
+			~double_vec(void);
+			void operator=(const double_vec& vec);
+			size_t size(void) const;
+			void resize(size_t length);
+			double& operator[](size_t i);
+			const double& operator[](size_t i) const;
+	};
+	
+
+
+	/* functions that make the wrapping a little more convenient */
+	bpn::array vector2array(const double_vec& vec);
+
+	/* general functions */
+	void Independent(bpn::array& x_array);
+
+// 	AD_double	(*cos_AD_double) 		( const AD_double & ) = &CppAD::cos;
+// 	AD_double *AD_double_mul_AD_double_AD_double(const AD_double &lhs, const AD_double &rhs);
+
+}
+
+
+
+
+
+
+
+
 
 BOOST_PYTHON_MODULE(_cppad)
 {
@@ -50,18 +95,19 @@ BOOST_PYTHON_MODULE(_cppad)
 	scope().attr("__doc__") =" CppAD: docstring \n\
 							   next line";
 
-	def("Independent",		Independent_numpy_AD_double);
+	def("Independent", &Independent);
 
 
 	class_<AD_double>("AD_double", init<double>())
 		.def(boost::python::self_ns::str(self))
-		.def("__mul__", AD_double_mul_AD_double_AD_double, return_value_policy<manage_new_object>())
-		.def("cos", cos_AD_double  )
-
+		.def(self*self)
+// 		.def("__mul__", AD_double_mul_AD_double_AD_double, return_value_policy<manage_new_object>())
+// 		.def("cos", cos_AD_double  )
 	;
-// 	class_<ADFun_double>("ADFun_double")
-// 	.def(init<const bpn::array&,const bpn::array&>())
-// 	;
+
+	class_<ADFun_double>("ADFun_double", init< bpn::array& , bpn::array& >())
+		.def("Forward", &ADFun_double::Forward)
+	;
 }
 
 #endif
