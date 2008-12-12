85117337d45fc3a0d82c4d5c26792836a4af4470
Sebastian Walter <sebastian.walter@gmail.com>
2008-12-12 09:45:14 +0100

    added some installation guide in the README

--- py_cppad.hpp
+++ py_cppad.hpp
@@ -52,6 +52,7 @@ BOOST_PYTHON_MODULE(_cppad)
 
 	def("Independent",		Independent_numpy_AD_double);
 
+
 	class_<AD_double>("AD_double", init<double>())
 		.def(boost::python::self_ns::str(self))
 		.def("__mul__", AD_double_mul_AD_double_AD_double, return_value_policy<manage_new_object>())
