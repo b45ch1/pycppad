2ca571285a10b2c5a0dee31e5d7fd683eec8a21e
Brad Bell <bradbell@brad-dell.(none)>
2008-12-14 14:53:23 -0800

    split level 1 and level 2 examples

---
+++ brad/example_1.py
@@ -0,0 +1,26 @@
+from python_cppad import *
+from numpy import array
+print 'Example using ad_double'
+# declare independent variable vector and start recording
+level = 1
+ad_x = array( [ ad_double(2) , ad_double(3) ] )
+independent(ad_x, level)
+# declare dependent variable vector and stop recording
+ad_y = array( [ 2. * ad_x[0] * ad_x[1] ] )
+f = adfun_double(ad_x, ad_y)
+print 'f(x0, x1) = 2. * x0 * x1'
+# evaluate the function at a different argument value
+p  = 0
+xp = array( [ 3. , 4. ] )
+fp = f.forward(p, xp)
+print 'f(3, 4)            = ', fp
+# evalute partial derivative with respect to first component
+p  = 1
+xp = array( [ 1. , 0. ] )
+fp = f.forward(p, xp)
+print 'partial_x0 f(3, 4) = ' , fp
+# evalute partial derivative with respect to second component
+p  = 1
+xp = array( [ 0. , 1. ] )
+fp = f.forward(p, xp)
+print 'partial_x1 f(3, 4) = ' , fp
--- brad/example_2.py
+++ brad/example_2.py
@@ -1,56 +1,43 @@
 from python_cppad import *
 from numpy import array
-# ---------------------------------------------------------
-print 'Example using ad_double------------------------------------'
+print 'Example using ad_ad_double'
+# declare level one independent variable vector and start level one recording
 level = 1
 ad_x = array( [ ad_double(2) , ad_double(3) ] )
 independent(ad_x, level)
-ad_y = array( [ 2. * ad_x[0] * ad_x[1] ] )
-f = adfun_double(ad_x, ad_y)
-print 'f(x0, x1) = 2. * x0 * x1'
-p  = 0
-xp = array( [ 3. , 4. ] )
-fp = f.forward(p, xp)
-print 'f(3, 4)            = ', fp
-p  = 1
-xp = array( [ 1. , 0. ] )
-fp = f.forward(p, xp)
-print 'partial_x0 f(3, 4) = ' , fp
-p  = 1
-xp = array( [ 0. , 1. ] )
-fp = f.forward(p, xp)
-print 'partial_x1 f(3, 4) = ' , fp
-print 'Example using ad_ad_double------------------------------------'
-level = 1
-ad_x = array( [ ad_double(2) , ad_double(3) ] )
-independent(ad_x, level)
-#
+# declare level two independent variable vector and start level two recording
 level = 2
 ad_ad_x = array( [ ad_ad_double(ad_x[0]) , ad_ad_double(ad_x[1]) ] )
 independent(ad_ad_x, level)
+# declare level 2 dependent variable vector and stop level 2 recording
 ad_ad_y = array( [ 2. * ad_ad_x[0] * ad_ad_x[1] ] )
-print 'f(x0, x1) = 2. * x0 * x1'
 ad_f = adfun_ad_double(ad_ad_x, ad_ad_y)
-#
+print 'f(x0, x1) = 2. * x0 * x1'
+# evaluate the function f(x) using level one independent variable vector
 p  = 0
 ad_fp = array( [ ad_double(0.) ] )  # kludge to pass back fp
 ad_f.forward(p, ad_x, ad_fp)
 print 'f(3, 4)            = ', ad_fp
+# evaluate the partial of f with respect to the first component
 p  = 1
 ad_xp = array( [ ad_double(1.) , ad_double(0.) ] )
 ad_f.forward(p, ad_xp, ad_fp)
 print 'partial_x0 f(3, 4) = ', ad_fp
-print 'g(x0, x1) = 2. * partial_x0 f(x0, x1) = 4 * x1'
+# declare level 1 dependent variable vector and stop level 1 recording 
 ad_y = 2. * ad_fp
 g = adfun_double(ad_x, ad_y)
+print 'g(x0, x1) = 2. * partial_x0 f(x0, x1) = 4 * x1'
+# evaluate the function g(x) at x = (4,5)
 p  = 0
 xp = array( [ 4. , 5. ] )
 gp = g.forward(p, xp)
 print 'g(4, 5)            = ', gp
+# evaluate the partial of g with respect to the first component
 p  = 1
 xp = array( [ 1. , 0. ] )
 gp = g.forward(p, xp)
 print 'partial_x0 g(4, 5) = ', gp
+# evaluate the partial of g with respect to the second component
 p  = 1
 xp = array( [ 0. , 1. ] )
 gp = g.forward(p, xp)
