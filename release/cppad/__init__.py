1d90bd00303cc35ecf942212e70bb21b637dba1f
Sebastian Walter <sebastian.walter@gmail.com>
2008-12-14 21:50:23 +0100

    added first version of the thin cppad wrapper to define convenient high level functions on the Python side and also do some error checking

--- release/cppad/__init__.py
+++ release/cppad/__init__.py
@@ -1,2 +1,27 @@
-from _cppad import * 
+"""\
+This is PyCppAD, a Python module to differentiate complex algorithms written in Python.
+PyCppAD is a wrapper to CppAD (C++).
+"""
+
+import numpy
+
 import _cppad
+from _cppad import AD_double
+
+
+
+def Independent(x):
+	"""
+	Mark a vector of AD_doubles as independent variables.
+	"""
+	if not isinstance(x,numpy.ndarray):
+		raise NotImplementedError('Input has to be of type numpy.array')
+	return _cppad.Independent(x)
+
+class ADFun_double(_cppad.ADFun_double):
+	"""
+	Create a function object.
+	"""
+	pass
+
+
