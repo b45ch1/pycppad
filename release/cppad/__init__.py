1b258ea80b8595775ad78005482a84ff1f777b31
Sebastian Walter <sebastian.walter@gmail.com>
2008-12-16 11:50:40 +0100

    added release/cppad/__init__.py

--- release/cppad/__init__.py
+++ release/cppad/__init__.py
@@ -4,24 +4,5 @@ PyCppAD is a wrapper to CppAD (C++).
 """
 
 import numpy
-
-import _cppad
-from _cppad import AD_double
-
-
-
-def Independent(x):
-	"""
-	Mark a vector of AD_doubles as independent variables.
-	"""
-	if not isinstance(x,numpy.ndarray):
-		raise NotImplementedError('Input has to be of type numpy.array')
-	return _cppad.Independent(x)
-
-class ADFun_double(_cppad.ADFun_double):
-	"""
-	Create a function object.
-	"""
-	pass
-
-
+import cppad
+from cppad import *
