00250b1184f8f25a430f8c0cc5bb69085687ff12
Sebastian Walter <sebastian.walter@gmail.com>
2008-12-17 16:46:47 +0100

    working ad and adfun functions

--- cppad.py
+++ cppad.py
@@ -15,99 +15,25 @@ def independent(x):
 	"""
 	Mark a vector of AD_doubles as independent variables.
 	"""
-	print 'called Independent'
 	if not isinstance(x,numpy.ndarray):
 		raise NotImplementedError('Input has to be of type numpy.array!')
 	
-	if x[0].__class__ == _cppad.AD_double:
-		print 'level = 1'
+	if isinstance(x[0],_cppad.AD_double):
 		return _cppad.Independent(x,1)
-	elif x[0].__class__ == _cppad.AD_AD_double:
-		print 'level = 2'
+	elif isinstance(x[0], _cppad.AD_AD_double):
 		return _cppad.Independent(x,2)
-	elif isinstance(x[0], ad):
-		return _cppad.Independent(x,x[0].level)
 	else:
 		raise NotImplementedError('Only multilevel taping up to 2 is currently implemented!')
 	
-
-class ad:
-	def __init__(self,x):
-		if type(x) == float:
-			self.level = 1
-			self.x  = adouble(x)
-
-		elif isinstance(x,ad):
-			if x.level == 1:
-				self.x = addouble(x.x)
-				self.level = x.level + 1
-
-		else:
-			raise ValueError("This type is not recognized!")
-
-	def __str__(self):
-		return str(self.x.value)
-
-	# CONDITIONALS
-	def __lt__(self,rhs):
-		return self.x < rhs.x
-
-	def __le__(self,rhs):
-		return self.x <= rhs.x
-
-	def __eq__(self,rhs):
-		return self.x == rhs.x
-
-	def __ne__(self,rhs):
-		return self.x != rhs.x
-
-	def __ge__(self,rhs):
-		return self.x >= rhs.x
-
-	def __gt__(self,rhs):
-		return self.x > rhs.x
-
-	# ELEMENTARY OPERATIONS
-	def __iadd__(self,rhs):
-		self.x += rhs.x
-		return self
-
-	def __isub__(self,rhs):
-		self.x -= rhs.x
-		return self
+def ad(x):
+	if numpy.isscalar(x):
+		return adouble(x)
+	elif isinstance(x,adouble):
+		return  addouble(x)
+	else:
+		raise NotImplementedError('Only multilevel taping up to 2 is currently implemented!')
 
 
-	def __imul__(self,rhs):
-		self.x *= rhs.x
-		return self
-
-	def __idiv__(self,rhs):
-		self.x /= rhs.x
-		return self
-	
-	def __add__(self,rhs):
-		return ad(self.x + rhs.x)
-
-	def __sub__(self,rhs):
-		return ad(self.x - rhs.x)
-
-	def __mul__(self,rhs):
-		return ad(self.x * rhs.x)
-
-	def __div__(self,rhs):
-		return (self.x / rhs.x)
-
-	def __radd__(self,lhs):
-		return self + lhs
-
-	def __rsub__(self,lhs):
-		return -self + lhs
-	
-	def __rmul__(self,lhs):
-		return self * lhs
-
-	def __rdiv__(self, lhs):
-		return lhs/self
 
 class adfun_double(_cppad.ADFun_double):
 	"""
@@ -121,3 +47,14 @@ class adfun_ad_double(_cppad.ADFun_AD_double):
 	Create a function object.
 	"""
 	pass
+
+
+def adfun(x,y):
+	if isinstance(x[0], adouble):
+		return adfun_double(x,y)
+	
+	elif isinstance(x[0], addouble):
+		return adfun_ad_double(x,y)
+
+	else:
+		raise NotImplementedError('Only multilevel taping up to 2 is currently implemented!')		
--- unit_tests/general.py
+++ unit_tests/general.py
@@ -122,135 +122,118 @@ def test_elementary_ad_operations():
 	assert x == ad(5.)
 	x -= y
 	assert x == ad(2.)	
+		
+def test_adouble_variable_info():
+	x = ad(2.)
+	y = ad(x)
+	print x
+	print y
+	print x
+	print x.value
+	print x.id
+	print x.taddr
 	
+	assert x.__str__() == '2'
+	assert x.value     == 2.
+	assert x.id        == 1
+	assert x.taddr     == 0
 	
+def test_ad_adouble_variable_info():
+	x = ad(ad(13.0))
+	print x
+	print x.value
+	print x.id
+	print x.taddr
 	
-	
-#def test_adouble_variable_info():
-	#x = ad(2.)
-	#y = addouble(x)
-	#print x
-	#print y
-	#assert False
-	#print x
-	#print x.value
-	#print x.id
-	#print x.taddr
-	
-	#assert x.__str__() == str(2.)
-	#assert x.value     == 2.
-	#assert x.id        == 1
-	#assert x.taddr     == 0
-	
-#def test_ad_adouble_variable_info():
-	#x = ad(ad(13.0))
-	#print x
-	#print x.value
-	#print x.id
-	#print x.taddr
-	
-	#assert x.__str__() ==  '13'
-	#assert x.value == 13.
-	#assert x.id == 1
-	#assert x.taddr == 0	
-	
-
-#def test_adouble():
-	#x = ad(3.)
-	#y = ad(3.)
-	#z = ad(4.)
-	
-	#assert x == y
-	#assert not x == z
-
+	assert x.__str__() ==  '13'
+	assert x.value == 13.
+	assert x.id == 1
+	assert x.taddr == 0	
 
 	
-#def test_deprecated_multi_level_taping_and_higher_order_derivatives():
-	#ok = True
-	#level = 1
-	#ad_x = numpy.array( [ ad_double(2) , ad_double(3) ] )
-	#independent(ad_x)
-	## declare level two independent variable vector and start level two recording
-	#level = 2
-	#ad_ad_x = numpy.array( [ ad_ad_double(ad_x[0]) , ad_ad_double(ad_x[1]) ] )
-	#independent(ad_ad_x)
-	## declare level 2 dependent variable vector and stop level 2 recording
-	#ad_ad_y = numpy.array( [ 2. * ad_ad_x[0] * ad_ad_x[1] ] )
-	#ad_f = adfun_ad_double(ad_ad_x, ad_ad_y) # f(x0, x1) = 2. * x0 * x1
-	## evaluate the function f(x) using level one independent variable vector
-	#p  = 0
-	#ad_fp = ad_f.Forward(p, ad_x)
-	#ok = ok and (ad_fp == 2. * ad_x[0] * ad_x[1])
-	## evaluate the partial of f with respect to the first component
-	#p  = 1
-	#ad_xp = numpy.array( [ ad_double(1.) , ad_double(0.) ] )
-	#ad_fp = ad_f.Forward(p, ad_xp)
-	#ok = ok and (ad_fp == 2. * ad_x[1])
-	## declare level 1 dependent variable vector and stop level 1 recording 
-	#ad_y = 2. * ad_fp
-	#g = adfun_double(ad_x, ad_y) # g(x0, x1) = 2. * partial_x0 f(x0, x1) = 4 * x1
-	## evaluate the function g(x) at x = (4,5)
-	#p  = 0
-	#x  = numpy.array( [ 4. , 5. ] )
-	#gp = g.Forward(p, x)
-	#ok = ok and (gp == 4. * x[1])
-	## evaluate the partial of g with respect to x0
-	#p  = 1
-	#xp = numpy.array( [ 1. , 0. ] )
-	#gp = g.Forward(p, xp)
-	#ok = ok and (gp == 0.)
-	## evaluate the partial of g with respect to x1
-	#p  = 1
-	#xp = numpy.array( [ 0. , 1. ] )
-	#gp = g.Forward(p, xp)
-	#ok = ok and (gp == 4.)
+def test_deprecated_multi_level_taping_and_higher_order_derivatives():
+	ok = True
+	level = 1
+	ad_x = numpy.array( [ adouble(2) , adouble(3) ] )
+	independent(ad_x)
+	# declare level two independent variable vector and start level two recording
+	level = 2
+	ad_ad_x = numpy.array( [ addouble(ad_x[0]) , addouble(ad_x[1]) ] )
+	independent(ad_ad_x)
+	# declare level 2 dependent variable vector and stop level 2 recording
+	ad_ad_y = numpy.array( [ 2. * ad_ad_x[0] * ad_ad_x[1] ] )
+	ad_f = adfun_ad_double(ad_ad_x, ad_ad_y) # f(x0, x1) = 2. * x0 * x1
+	# evaluate the function f(x) using level one independent variable vector
+	p  = 0
+	ad_fp = ad_f.Forward(p, ad_x)
+	ok = ok and (ad_fp == 2. * ad_x[0] * ad_x[1])
+	# evaluate the partial of f with respect to the first component
+	p  = 1
+	ad_xp = numpy.array( [ adouble(1.) , adouble(0.) ] )
+	ad_fp = ad_f.Forward(p, ad_xp)
+	ok = ok and (ad_fp == 2. * ad_x[1])
+	# declare level 1 dependent variable vector and stop level 1 recording 
+	ad_y = 2. * ad_fp
+	g = adfun_double(ad_x, ad_y) # g(x0, x1) = 2. * partial_x0 f(x0, x1) = 4 * x1
+	# evaluate the function g(x) at x = (4,5)
+	p  = 0
+	x  = numpy.array( [ 4. , 5. ] )
+	gp = g.Forward(p, x)
+	ok = ok and (gp == 4. * x[1])
+	# evaluate the partial of g with respect to x0
+	p  = 1
+	xp = numpy.array( [ 1. , 0. ] )
+	gp = g.Forward(p, xp)
+	ok = ok and (gp == 0.)
+	# evaluate the partial of g with respect to x1
+	p  = 1
+	xp = numpy.array( [ 0. , 1. ] )
+	gp = g.Forward(p, xp)
+	ok = ok and (gp == 4.)
 	
-	#assert ok
+	assert ok
 
-#def test_multi_level_taping_and_higher_order_derivatives():
-	#ok = True
-	#level = 1
-	#ad_x = numpy.array( [ ad(2) , ad(3) ] )
-	#print ad_x
-	#independent(ad_x)
-	## declare level two independent variable vector and start level two recording
-	#level = 2
-	#ad_ad_x = numpy.array( [ ad(ad_x[0]) , ad(ad_x[1]) ] )
-	#print ad_ad_x
-	#independent(ad_ad_x)
-	## declare level 2 dependent variable vector and stop level 2 recording
-	#ad_ad_y = numpy.array( [ 2. * ad_ad_x[0] * ad_ad_x[1] ] )
-	#print ad_ad_y
-	##ad_f = adfun_ad_double(ad_ad_x, ad_ad_y) # f(x0, x1) = 2. * x0 * x1
-	### evaluate the function f(x) using level one independent variable vector
-	##p  = 0
-	##ad_fp = ad_f.Forward(p, ad_x)
-	##ok = ok and (ad_fp == 2. * ad_x[0] * ad_x[1])
-	### evaluate the partial of f with respect to the first component
-	##p  = 1
-	##ad_xp = numpy.array( [ ad(1.) , ad(0.) ] )
-	##ad_fp = ad_f.Forward(p, ad_xp)
-	##ok = ok and (ad_fp == 2. * ad_x[1])
-	### declare level 1 dependent variable vector and stop level 1 recording 
-	##ad_y = 2. * ad_fp
-	##g = adfun_double(ad_x, ad_y) # g(x0, x1) = 2. * partial_x0 f(x0, x1) = 4 * x1
-	### evaluate the function g(x) at x = (4,5)
-	##p  = 0
-	##x  = numpy.array( [ 4. , 5. ] )
-	##gp = g.Forward(p, x)
-	##ok = ok and (gp == 4. * x[1])
-	### evaluate the partial of g with respect to x0
-	##p  = 1
-	##xp = numpy.array( [ 1. , 0. ] )
-	##gp = g.Forward(p, xp)
-	##ok = ok and (gp == 0.)
-	### evaluate the partial of g with respect to x1
-	##p  = 1
-	##xp = numpy.array( [ 0. , 1. ] )
-	##gp = g.Forward(p, xp)
-	##ok = ok and (gp == 4.)
-	#assert False
-	#assert ok
+def test_multi_level_taping_and_higher_order_derivatives():
+	ok = True
+	level = 1
+	ad_x = numpy.array( [ ad(2) , ad(3) ] )
+	independent(ad_x)
+	# declare level two independent variable vector and start level two recording
+	level = 2
+	ad_ad_x = numpy.array( [ ad(ad_x[0]) , ad(ad_x[1]) ] )
+	independent(ad_ad_x)
+	# declare level 2 dependent variable vector and stop level 2 recording
+	ad_ad_y = numpy.array( [ 2. * ad_ad_x[0] * ad_ad_x[1] ] )
+	ad_f = adfun(ad_ad_x, ad_ad_y) # f(x0, x1) = 2. * x0 * x1
+	# evaluate the function f(x) using level one independent variable vector
+	p  = 0
+	ad_fp = ad_f.Forward(p, ad_x)
+	ok = ok and (ad_fp == 2. * ad_x[0] * ad_x[1])
+	# evaluate the partial of f with respect to the first component
+	p  = 1
+	ad_xp = numpy.array( [ ad(1.) , ad(0.) ] )
+	ad_fp = ad_f.Forward(p, ad_xp)
+	ok = ok and (ad_fp == 2. * ad_x[1])
+	# declare level 1 dependent variable vector and stop level 1 recording 
+	ad_y = 2. * ad_fp
+	g = adfun(ad_x, ad_y) # g(x0, x1) = 2. * partial_x0 f(x0, x1) = 4 * x1
+	# evaluate the function g(x) at x = (4,5)
+	p  = 0
+	x  = numpy.array( [ 4. , 5. ] )
+	gp = g.Forward(p, x)
+	ok = ok and (gp == 4. * x[1])
+	# evaluate the partial of g with respect to x0
+	p  = 1
+	xp = numpy.array( [ 1. , 0. ] )
+	gp = g.Forward(p, xp)
+	ok = ok and (gp == 0.)
+	# evaluate the partial of g with respect to x1
+	p  = 1
+	xp = numpy.array( [ 0. , 1. ] )
+	gp = g.Forward(p, xp)
+	ok = ok and (gp == 4.)
+	
+	assert ok
 
 
 
