a8c2e52b9f4641e0d930b8f9784709a77a968f7c
Brad Bell <bradbell@brad-dell.(none)>
2009-01-06 05:21:29 -0800

    pycppad/brad: add adfun documentation

--- brad/example/independent.py
+++ brad/example/independent.py
@@ -13,11 +13,16 @@
 from cppad import *
 def test_independent() :
   x   = array( [ 0., 0., 0. ] )
-  a_x = independent(x)    # declare independent variables and start recording
+  a_x = independent(x)    # level 1 independent variables and start recording
+  a2x = independent(a_x)  # level 2 independent variables and start recording
   assert type(a_x) == numpy.ndarray
+  assert type(a2x) == numpy.ndarray
   for j in range(len(x)) :
     assert isinstance(x[j],   float)
     assert isinstance(a_x[j], a_float)
+    assert isinstance(a2x[j], a2float)
     assert a_x[j] == x[j]
-  f   = adfun(a_x, a_x)   # stop recording
+    assert a2x[j] == x[j]
+  a_f = adfun(a2x, a2x)   # stop level 2 recording
+  f   = adfun(a_x, a_x)   # stop levle 1 recording
 # END CODE
