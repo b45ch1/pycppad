e8fdae7fb26a44e350ab614913a9eed9dd7380de
Brad Bell <bradbell@brad-dell.(none)>
2009-01-07 04:33:55 -0800

    comment out segmentation fault and focus on problem with x ** y

--- unit_tests/issues.py
+++ unit_tests/issues.py
@@ -4,25 +4,18 @@ sys.path = ['.'] + sys.path #adding current working directory to the $PYTHONPATH
 from cppad import *
 import numpy
 
-# Comment out these test because 
-# segmentation fault prevents proper error reporting
-#
-#def test_numpy_array_data_type() :
-#	my_list  = [1 , 2 ]
-#	my_array = numpy.asarray(my_list, dtype=float)
-#	
-#	# this works, but type( my_array[0] ) != float
-#	assert isinstance(my_array[0], float)
-#
-#def test_int_pow_a_double() :
-#	x = 1
-#	y = ad(1)
-#	assert x**y == 1
+def test_int_pow_a_double() :
+	x = 1
+	y = ad(1)
+	assert x**y == 1
 	
 	
-def test_independent_exception_handling():
-	""" this results in a segmentation fault"""
-	a_x = numpy.array( [ad(1.)] )
-	independent(a_x)
-	a_y = a_x
-	f = adfun(a_x, a_y)
+# The code above violates the specifications for CppAD. If it is run
+# with debugging, CppAD generates an error message. If run without debugging
+# CppAD assumes that its specifications have been met.
+#def test_independent_exception_handling():
+#	""" this results in a segmentation fault if run without debugging"""
+#	a_x = numpy.array( [ad(1.)] )
+#	independent(a_x)
+#	a_y = a_x
+#	f = adfun(a_x, a_y)
