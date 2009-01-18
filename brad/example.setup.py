6470cc8df4e98b5db8789d3c41f6f9dbfd93e92c
Brad Bell <bradbell@brad-dell.(none)>
2009-01-17 16:51:21 -0800

    brad: clean up setup.py

--- brad/example.setup.py
+++ brad/example.setup.py
@@ -1,19 +1,15 @@
 #!/usr/bin/env python
 # ---------------------------------------------------------------------
-# build with: $ python setup.py build_ext --inplace
-# clean with: # python setup.py clean --all
+# Copy this file to setup.py, change settings below to work for your system, 
+# build with: ./setup.py build_ext --inplace
+# clean with: ./setup.py clean --all
 cppad_include_dir = '../cppad-20081128'
 boost_lib_dir     = '/usr/lib'
 boost_python_lib  = 'boost_python'
 # ---------------------------------------------------------------------
-
-
-# IT IS USUALLY NOT NECESSARY TO CHANGE ANTHING BELOW THIS POINT
 # see:
 # http://www.scipy.org/Documentation/numpy_distutils
 # http://docs.cython.org/docs/tutorial.html
-
-
 
 from numpy.distutils.misc_util import get_numpy_include_dirs
 from numpy.distutils.misc_util import Configuration
@@ -23,55 +19,50 @@ from numpy.distutils.core import Command
 # override default setup.py help output
 import sys
 if len(sys.argv) == 1:
-	print """
+  print """
+Options:
+1: build the extension with
+   python ./setup.py build_ext --inplace
 
-	You didn't enter what to do!
+2: remove generated files with
+   python ./setup.py clean --all
 
-	Options:
-	1: build the extension with
-	   python setup.py build_ext --inplace
-
-	2: remove generated files with
-	   python setup.py clean --all
-
-
-	Remark: This is an override of the default behaviour of the distutils setup.
-	"""
-	exit()
+  """
+  exit()
 
 
 class clean(Command):
-	"""
-	This class is used in numpy.distutils.core.setup.
-	When $python setup.py clean is called, an instance of this class is created and then it's run method is called.
-	"""
+  """
+This class is used in numpy.distutils.core.setup.
+When $python setup.py clean is called, an instance of this class is created 
+and then it's run method is called.
+  """
 
-	description = "Clean everything"
-	user_options = [("all","a","the same")]
+  description = "Clean everything"
+  user_options = [("all","a","the same")]
 
-	def initialize_options(self):
-		self.all = None
+  def initialize_options(self):
+    self.all = None
 
-	def finalize_options(self):
-		pass
+  def finalize_options(self):
+    pass
 
-	def run(self):
-		import os
-		os.system("rm -rf build")
-		os.system("rm -rf *.o")
-		os.system("rm -rf cppad_.so")
-		os.system("rm -rf *.pyc")
+  def run(self):
+    import os
+    os.system("rm -rf build")
+    os.system("rm -rf cppad_.so")
+    os.system("rm -rf *.pyc")
 
 
 config = Configuration('',parent_package='',top_path='')
 config.add_extension(
-	name = 'cppad_',
-	sources = ['adfun.cpp', 'pycppad.cpp', 'vec2array.cpp', 'vector.cpp'],
-	include_dirs = [ get_numpy_include_dirs(), cppad_include_dir ],
-	extra_compile_args = [ ],
-	library_dirs = [ boost_lib_dir ],
-	runtime_library_dirs = [ boost_lib_dir ] ,
-	libraries = [ boost_python_lib ]
+  name         = 'cppad_',
+  sources      = ['adfun.cpp', 'pycppad.cpp', 'vec2array.cpp', 'vector.cpp'],
+  include_dirs = [ get_numpy_include_dirs(), cppad_include_dir ],
+  extra_compile_args   = [ ],
+  library_dirs         = [ boost_lib_dir ],
+  runtime_library_dirs = [ boost_lib_dir ] ,
+  libraries            = [ boost_python_lib ]
 )
 #include_dirs, define_macros, undef_macros, library_dirs, libraries, runtime_library_dirs, extra_objects, extra_compile_args, extra_link_args, export_symbols, swig_opts, depends, language, f2py_options, module_dirs, extra_info.
 
@@ -80,4 +71,3 @@ options_dict['cmdclass'] = {'clean':clean}
 #, cmdclass = {'clean':clean}
 #setup(**config.todict())
 setup(**options_dict)
-
