c7be27801796f9f63c404da2021b1ef88a4065e0
Bradley M. Bell <bradbell@localhost.localdomain>
2010-01-29 06:56:48 -0800

    file to make commits

---
+++ commit.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+#
+# replace text in msg for this commit
+msg="SUMMARY COMMENT ENDING WIHT A ;
+
+FILE_TO_COMMIT: COMMMENT ENDING WITH A ;
+...
+FILE_TO_COMMIT: COMMMENT ENDING WITH A ;
+"
+# -----------------------------------------------------------------------
+list=`echo $msg | sed -e 's|^[^;]*;||' -e 's|:[^;]*;||g'`
+msg=`echo $msg | sed -e 's|;|.\n|g'`
+echo "git commit \\"
+echo "\"$msg\" \\"
+echo "$list"
+read -p "is this ok [y/n] ?" response
+if [ "$response" != "y" ]
+then
+	exit 1
+fi
+git commit -m "$msg" $list
