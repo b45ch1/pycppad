d6f6233b7b5492ace00bb9d26b8e3ee747878542
Bradley M. Bell <bradbell@localhost.localdomain>
2010-01-30 06:49:48 -0800

    fix editing of comment

--- commit.sh
+++ commit.sh
@@ -1,16 +1,17 @@
 #! /bin/bash
 #
 # replace text in msg for this commit
-msg="SUMMARY COMMENT ENDING WIHT A ;
+msg="summary_commnet_ending_with_semicolon;
 
-FILE_TO_COMMIT: COMMMENT ENDING WITH A ;
+file_to_commit:comment_for_this_file_ending_with_semicolon;
 ...
-FILE_TO_COMMIT: COMMMENT ENDING WITH A ;
+file_to_commit:comment_for_this_file_ending_with_semicolon;
 "
 # -----------------------------------------------------------------------
 list=`echo $msg | sed -e 's|^[^;]*;||' -e 's|:[^;]*;||g'`
-msg=`echo $msg | \
-sed -e 's|omh/||g' -e 's|pycppad/||g' -e 's|example/||g' -e 's|;|.\n|g'` echo "git commit \\"
+msg=`echo $msg | sed -e 's|omh/||g' -e 's|pycppad/||g' \
+	-e 's|example/||g' -e 's|;|.\n|g'` 
+echo "git commit \\"
 echo "\"$msg\" \\"
 echo "$list"
 read -p "is this ok [y/n] ?" response
